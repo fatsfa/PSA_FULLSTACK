be2118475d0789d166f4151600765b2ceacd444c
fatsfa <fathifathima808@gmail.com>
2024-06-13 20:04:43 +0530

    test

--- JAVA/Basics/One.java
+++ JAVA/Basics/One.java
@@ -1,3 +1,5 @@
 Class Test{
+    public static void main(String args[])
+    system.out.println("hy");
     
 }
---
+++ JS/PART2/EventOne.js
@@ -0,0 +1,7 @@
+function chageColor(){
+ 
+    let btn_Ref= document.getElementsByTagName('button')[0]
+    console.log(btn_Ref)
+    btn_Ref.style.backgroundColor='green'
+
+}
